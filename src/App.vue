0ce250c90b056d47611622d95bf00bb079ee561d
gsusgit <fernandezjodar.jesus@gmail.com>
2024-09-27 08:30:02 +0200

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
-<script setup>
-</script>
-
 <template>
   <div class="flex flex-col min-h-screen">
     <main class="flex-grow">
       <div class="max-w-screen-xl mx-auto p-4 my-20">
-        <h1 class="mb-5 text-xl">PrimeVue</h1>
         <form>
           <div class="mb-5">
             <FloatLabel>
@@ -18,5 +14,4 @@
       </div>
     </main>
   </div>
-  <div class="mx-auto"></div>
 </template>
